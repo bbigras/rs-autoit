1c6d89e06f10737a59ae159e75c19ebb4eff918a
Bruno Bigras <bigras.bruno@gmail.com>
2017-01-24 23:57:33 -0500

    fix 'allow(dead_code)' macro

--- src/lib.rs
+++ src/lib.rs
@@ -6,7 +6,7 @@ mod bindings {
     #![allow(non_camel_case_types)]
     #![allow(non_snake_case)]
 
-    #[allow(dead_code)]
+    #![allow(dead_code)]
 
     include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
 }
